f2ba195f04ffb0647b4f4159cd14ab892cf7ac1c
danizaghian <dani.zaghian@gmail.com>
2017-09-06 21:31:48 -0700

    added model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,27 @@
 class Product < ActiveRecord::Base
+
+  def sale_message
+    if price.to_f <= 2
+      "Discount Item!"
+    else
+      "Everyday Value!"
+    end
+  end
+
+  def sale_message_class_name
+    if sale_message == "Discount Item!"
+      "discount-item"
+    else
+      ""
+    end
+  end
+
+  def tax
+    price.to_f * 0.09
+  end
+
+  def total
+    price.to_f + tax
+  end
+
 end
