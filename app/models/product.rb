041d6ebcfd4945bfd26536c87095b5c6402591d0
danizaghian <dani.zaghian@gmail.com>
2017-09-07 18:22:39 -0700

    model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,20 @@
 class Product < ActiveRecord::Base
 
-  def sale_message
-    if price.to_f <= 2
-      "Discount Item!"
-    else
-      "Everyday Value!"
-    end
-  end
+  # def sale_message
+  #   if price.to_f <= 2
+  #     "Discount Item!"
+  #   else
+  #     "Everyday Value!"
+  #   end
+  # end
 
-  def sale_message_class_name
-    if sale_message == "Discount Item!"
-      "discount-item"
-    else
-      ""
-    end
-  end
+  # def sale_message_class_name
+  #   if sale_message == "Discount Item!"
+  #     "discount-item"
+  #   else
+  #     ""
+  #   end
+  # end
 
   def tax
     price.to_f * 0.09
@@ -22,6 +22,10 @@ class Product < ActiveRecord::Base
 
   def total
     price.to_f + tax
+  end
+
+  def discounted?
+    price.to_f <=2
   end
 
 end
